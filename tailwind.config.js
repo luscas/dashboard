cee33457076e32ca58fc9e4f7011d58642a8b68c
Lucas Paz <eu@lucaspaz.com>
2021-01-23 03:49:22 -0300

    ...

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
